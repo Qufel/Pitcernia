ac5abc4563b03d903cde819fbcf39909cedc7070
Qufel <97023524+Qufel@users.noreply.github.com>
2023-03-23 20:29:24 +0100

    Naprawienie błędu z nieładującym się css (raczej)

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,10 +3,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="./styles/style.css" media="all">
-    <!-- <link rel="stylesheet" href="./styles/animations.css" media="all"> -->
-    <link rel="stylesheet" href="./styles/themes.css" media="all">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" media="all">
+    <link rel="stylesheet" type="text/css" href="./styles/style.css" media="all">
+    <link rel="stylesheet" type="text/css" href="./styles/themes.css" media="all">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" media="all">
     
     <script defer async src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script defer async src="./bootstrap/dist/js/bootstrap.min.js"></script>
