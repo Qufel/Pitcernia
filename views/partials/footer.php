e559ed797329bafe4b5b4c3c064844e3734212d9
Qufel <97023524+Qufel@users.noreply.github.com>
2023-04-11 15:22:20 +0200

    Zmiany w stopce, niektóre rzeczy do poprawy

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,27 +1,14 @@
 <footer class=" text-center">
-    <!-- Grid container -->
-    <div class="container p-5 pb-0">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-            <!-- Facebook -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="bi bi-facebook"></i>
-            </a>
-
-            <!-- Instagram -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-instagram"></i></a>
-
-            <!-- Github -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-github"></i></a>
+    <div class="socials container pb-0">
+        <section class="">
+            <a class="btn btn-outline-light btn-floating m-1" aria-label="facebook-link" href="https://www.facebook.com" role="button"><i class="bi bi-facebook"></i></a>
+            <a class="btn btn-outline-light btn-floating m-1" aria-label="instagram-link" href="https://www.instagram.com" role="button"><i class="bi bi-instagram"></i></a>
+            <a class="btn btn-outline-light btn-floating m-1" aria-label="github-link" href="https://www.github.com" role="button"><i class="bi bi-github"></i></a>
         </section>
-        <!-- Section: Social media -->
     </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
-    <div class="text-center p-3" img="pitcernia\assets\backgrounds\footer-bg.png">
-       Pitcernia ©
-        <a class="text-white" href="https://rroll.to/WQhc2H">pitcernia.ezyro.com</a>
+    <div class="p-md-2 text-light">
+        Pitcernia©
+        <script>document.write(new Date().getFullYear());</script>
+        <a class="text-white" href="https://rroll.to/Vpsybr">pitcernia.ezyro.com</a>
     </div>
-    <!-- Copyright -->
 </footer>
